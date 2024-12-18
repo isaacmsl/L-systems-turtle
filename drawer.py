c5b7a2a97ff6db2b74bc9d75ff770fdd788c6db3
Gustavo Gorgônio <ggorgon03@gmail.com>
2024-12-18 02:38:42 -0300

    chaging drawer to run in command line

--- drawer.py
+++ drawer.py
@@ -1,11 +1,39 @@
+import argparse
 from lsdm import LSystemDrawerMenager
-lsdm = LSystemDrawerMenager()
-file = "./testes/in9.txt"
-iterations = 5
-lsdm.read(file)
-lsdm.generate(iterations)
-if lsdm.check(iterations):
-    print(f"A string '{lsdm.log}' pertence a linguagem descrita por '{file}'")
-else:
-    print(f"A string '{lsdm.log}' NÃO pertence a linguagem descrita por '{file}'")
-lsdm.draw(True)
+
+def main():
+    # Configurar o parser de argumentos
+    parser = argparse.ArgumentParser(description='Processa um arquivo de configuração de L-System e desenha a representação.')
+    parser.add_argument('path',
+                        type=str,
+                        nargs='?',
+                        default='./input_files/in2.txt',
+                        help='O caminho para o arquivo de configuração da L-System.'
+    )
+    parser.add_argument('iterations',
+                        type=int,
+                        nargs='?',
+                        default=3,
+                        help='O número de iterações para gerar a sequência do L-System.'
+    )
+    parser.add_argument('--trace',
+                        action='store_false',
+                        help='Indica se o desenho deve ser feito passo a passo.'
+    )
+    
+    args = parser.parse_args()
+    
+    # Inicializar o gerenciador de L-System
+    lsdm = LSystemDrawerMenager()
+    # Ler o arquivo de configuração
+    lsdm.read(args.path)
+    # Gerar a sequência do L-System
+    lsdm.generate(iterations=args.iterations)
+    # Desenhar o L-System
+    try:
+        lsdm.draw(trace=args.trace)
+    except:
+        pass
+
+if __name__ == '__main__':
+    main()
